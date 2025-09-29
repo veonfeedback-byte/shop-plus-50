e00bd8d417c4d590fb0d510da7dad590ed4a3cbc
veonfeedback-byte <veon.feedback@gmail.com>
2025-09-29 20:13:52 +0500

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -874,7 +874,7 @@ export default function HomePage() {
           </>
             ) : (
             <section>
-            <div className="fixed top-[120px] left-0 right-0 z-20 bg-white shadow-sm border-b border-gray-200">
+            <div className="w-screen -ml-[calc((100vw-100%)/2)]sticky top-[120px] z-20 bg-white shadow-sm border-b border-gray-200">
               <div className="flex gap-3 overflow-x-auto px-3 py-2 scrollbar-hide">
                 {filterTabs.map((tab) => (
                   <button
