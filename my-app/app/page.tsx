f2d8cdbdd350d299240b9d957b1ca8c1e278db6e
veonfeedback-byte <veon.feedback@gmail.com>
2025-10-01 02:06:49 +0500

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -898,14 +898,15 @@ export default function HomePage() {
             <section>
            
             {/* Trending Header */}
-          <div className="flex items-center gap-2 mt-8 pl-0 pr-6 py-2 rounded-r-2xl shadow-sm
-                          bg-gradient-to-r from-gray-100 via-gray-50 to-transparent
-                          border-l-4 border-red-400">
-            <Flame className="w-5 h-5 text-red-500" />
-            <span className="text-base sm:text-lg font-medium text-gray-700 tracking-wide">
-              Trending
-            </span>
-          </div>
+            <div className="flex items-center gap-2 mt-8 w-1/2 pl-3 pr-6 py-2 rounded-r-2xl shadow-sm
+                            bg-gradient-to-r from-gray-200 via-gray-100 to-gray-50
+                            border-l-4 border-red-400">
+              <Flame className="w-5 h-5 text-red-500" />
+              <span className="text-base sm:text-lg font-medium text-gray-700 tracking-wide">
+                Trending
+              </span>
+            </div>
+
 
             <div className="grid grid-cols-2 md:grid-cols-4 gap-2 sm:gap-3 mt-4">
               {homeProducts.slice(0, visibleHome).map((p, idx) => (
